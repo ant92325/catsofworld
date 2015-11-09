eb9487d4f3bcc207e8d2579b84cf44a14c4d14eb
ant92325 <ant92325@gmail.com>
2015-11-09 10:46:24 +0200

    changed another version

---
+++ config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  instagram: {
+    client: "8cd7828c5bf049e7bf918e2766b2ca51",
+    secret: "7c2c55c0eace4d30a9add627710bce43",
+    verify: "7c2c55c0eace4d30a9add627710bce43"
+  },
+
+  database: {
+    host: "rethinkdb-colify.cloudapp.net",
+    port: 28015,
+    db: "cats"
+  },
+
+  host: "4422555f.ngrok.com",
+  port: 8091
+}
